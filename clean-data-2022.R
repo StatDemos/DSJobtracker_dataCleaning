becc77d04de870d443fff0c65266b0fd799e4140
trishiii <wickramarathnetrishika@gmail.com>
2023-08-08 11:47:22 +0530

    Updated code

--- clean-data-2022.R
+++ clean-data-2022.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(readxl)
+library(tm)
 
 data <- read_csv("DSjobtracker_2022.csv")
 
 # Filter the columns
 
-column_names <- c("Job_Level", "Industry", "Duplicate/Unique", "Experience UL (in years)", 
-                  "Employment type",  
+column_names <- c("Job_Level", "Duplicate/Unique", "Experience UL (in years)", 
+                  "Employment type",
                   "Work Setting", "Salary UL", "Modelling", "VBA macro", "EDA",                                                 
                   "Hypothesis Testing", "Data Analytics", "Statistics",                                           
                   "Data Cleaning", "Survey Design",  "Dashboard Creation",                                   
@@ -24,7 +25,7 @@ column_names <- c("Job_Level", "Industry", "Duplicate/Unique", "Experience UL (i
                   "Experience with remote sensing", "Shell", "Go", "PyTorch",                                             
                   "ArcGIS", "PostGIS", "STATA", "Leadership", "Collaboration Skills",                                 
                   "Research Skills", "Crystal Reports", "CIMA", "ACCA", "CIM",                                                   
-                  "CA", "CFA", "Chartered Engineer", "Currency", "Additional_languages",
+                  "CA", "CFA", "Chartered Engineer",
                   "Educational qualifications (if they need more than 1)", "Diploma",
                   "Mphil", "Data_translation")
 
@@ -32,13 +33,13 @@ data1 <- data %>% select(-column_names)
 
 # Renaming column names
 
-data1 <- data1 %>% rename("Location"="Country", "Salary"="Salary LL", 
+data1 <- data1 %>% rename( "Salary"="Salary LL", 
                          "English Needed"="English proficiency", "BSc_needed"="BSc",
-                         "MSc_needed"="MSc", "PhD_needed"="Dphil/PhD",
-                         "Job_Category"="Job_Field")
+                         "MSc_needed"="MSc", "PhD_needed"="Dphil/PhD","Job_Field"="Industry", "Minimum Experience in Years"="Experience LL (in years)","Job_Category"="Job_Field"
+                        )
 
-data1 <- data1 %>% mutate(Knowledge_in = rep("NA", 360), year = rep(2022,360),
-                          Educational_qualifications = rep("NA", 360))
+data1 <- data1 %>% mutate(Knowledge_in = rep(NA, 360), year = rep(2022,360),`English proficiency description` = rep(NA,360),
+                          Educational_qualifications = rep(NA, 360),Location=rep(NA,360))
 
 
 dim(data1)
@@ -48,17 +49,17 @@ dim(data1)
 
 # Categorizing experience variable
 
-data1 <- data1 %>% mutate(Experience_Category = case_when(
-  `Experience LL (in years)` %in% c(0,0.5,1,2) ~ "Two or less years",
-  `Experience LL (in years)` %in% c(3,4,5) ~ "More than 2 and less than 5 years",
-  `Experience LL (in years)` %in% c(6:10) ~ "More than 5 and less than 10 years",
-  `Experience LL (in years)` %in% c(11,12) ~ "More than 10 years",
-  `Experience LL (in years)` %in% c("NA", NA) ~ "Unknown or Not needed"
-  
-))
+# data1 <- data1 %>% mutate(Experience_Category = case_when(
+#   `Experience LL (in years)` %in% c(0,0.5,1,2) ~ "Two or less years",
+#   `Experience LL (in years)` %in% c(3,4,5) ~ "More than 2 and less than 5 years",
+#   `Experience LL (in years)` %in% c(6:10) ~ "More than 5 and less than 10 years",
+#   `Experience LL (in years)` %in% c(11,12) ~ "More than 10 years",
+#   `Experience LL (in years)` %in% c("NA", NA) ~ "Unknown or Not needed"
+#   
+# ))
 
 # Removing experience column
-data1 <- data1 %>% select(-`Experience LL (in years)`)
+#data1 <- data1 %>% select(-`Experience LL (in years)`)
 
 
 # Categorizing payment frequency
@@ -75,47 +76,105 @@ data1 <- data1 %>% mutate(`Payment Frequency` = case_when(
 
 # Categorizing location column
 
-data1 <- data1 %>% mutate(Location = case_when(
-  Location %in% c("Sri Lanka", "Sri lanka") ~ "Sri Lanka",
-  Location %in% c("NA", NA, "EMEA", "South Asia") ~ NA,
-  Location %in% c("UAE", "Dubai, UAE") ~ "United Arab Emirates",
-  Location %in% c("United States", "United States of America") ~ "USA",
-  Location %in% c("United Kingdom", "England", "Ireland") ~ "UK",
-  Location %in% c("Austra", "Austrailia") ~ "Australia",
-  Location == "Singapore" ~ "Singapore",
-  Location == "New Zealand" ~ "New Zealand",
-  Location == "Tunisia" ~ "Tunisia",
-  Location == "Japan" ~ "Japan",
-  Location == "Denmark" ~ "Denmark",
-  Location == "Hungary" ~ "Hungary",
-  Location == "India" ~ "India",
-  Location == "Germany" ~ "Germany",
-  Location == "Greece" ~ "Greece",
-  Location == "Lithuania" ~ "Lithuania",
-  Location == "Sweden" ~ "Sweden",
-  Location == "Poland" ~ "Poland",
-  Location == "Malaysia" ~ "Malaysia",
-  Location == "Spain" ~ "Spain",
-  Location == "Belgium" ~ "Belgium",
-  Location == "Vietnam, Thailand, Indonesia" ~ "Thailand",
-  Location == "Ecuador" ~ "Ecuador",
-  Location == "Netherlands" ~ "Netherlands",
-  Location == "Finland" ~ "Finland",
-  Location == "Romania" ~ "Romania",
-  Location == "Mexico" ~ "Mexico",
-  Location == "Italy" ~ "Italy",
-  Location == "Virginia" ~ "Virginia",
-  Location == "Czech Republic" ~ "Czech Republic",
-  Location == "Turkey" ~ "Turkey",
-  Location == "China" ~ "China",
-  Location == "Canada" ~ "Canada",
-  Location == "Switzerland" ~ "Switzerland",
-  Location == "Russia" ~ "Russia",
-  Location == "Malta" ~ "Malta",
-  Location == "Qatar" ~ "Qatar",
-  Location == "Portugal" ~ "Portugal",
-  Location == "Bulgaria" ~ "Bulgaria"
+data1 <- data1 %>% mutate(Country = case_when(
+ Country %in% c("Sri Lanka", "Sri lanka") ~ "Sri Lanka",
+ Country %in% c("NA", NA, "EMEA", "South Asia") ~ NA,
+ Country %in% c("UAE", "Dubai, UAE") ~ "United Arab Emirates",
+ Country %in% c("USA", "United States of America") ~ "United States",
+ Country %in% c("UK", "England", "Ireland") ~ "United Kingdom",
+ Country %in% c("Austra", "Austrailia") ~ "Australia",
+ Country == "Singapore" ~ "Singapore",
+ Country == "New Zealand" ~ "New Zealand",
+ Country == "Tunisia" ~ "Tunisia",
+ Country == "Japan" ~ "Japan",
+ Country == "Denmark" ~ "Denmark",
+ Country == "Hungary" ~ "Hungary",
+ Country == "India" ~ "India",
+ Country == "Germany" ~ "Germany",
+ Country == "Greece" ~ "Greece",
+ Country == "Lithuania" ~ "Lithuania",
+ Country == "Sweden" ~ "Sweden",
+ Country == "Poland" ~ "Poland",
+ Country == "Malaysia" ~ "Malaysia",
+ Country == "Spain" ~ "Spain",
+ Country == "Belgium" ~ "Belgium",
+ Country == "Vietnam, Thailand, Indonesia" ~ "Thailand",
+ Country == "Ecuador" ~ "Ecuador",
+ Country == "Netherlands" ~ "Netherlands",
+ Country == "Finland" ~ "Finland",
+ Country == "Romania" ~ "Romania",
+ Country == "Mexico" ~ "Mexico",
+ Country == "Italy" ~ "Italy",
+ Country == "Virginia" ~ "Virginia",
+ Country == "Czech Republic" ~ "Czech Republic",
+ Country == "Turkey" ~ "Turkey",
+ Country == "China" ~ "China",
+ Country == "Canada" ~ "Canada",
+ Country == "Switzerland" ~ "Switzerland",
+ Country == "Russia" ~ "Russia",
+ Country == "Malta" ~ "Malta",
+ Country == "Qatar" ~ "Qatar",
+ Country == "Portugal" ~ "Portugal",
+ Country == "Bulgaria" ~ "Bulgaria"
 ))
+
+
+# Country code
+
+data1 <- data1 %>% mutate(country_code = case_when(
+  Country == "United States"~ "USA",
+  Country == "Sri Lanka"~ "LKA",
+  Country == "Australia"~ "AUS",
+  Country == "United Kingdom"~ "GBR",
+  Country == "Canada"~ "CAN",
+  Country == "United Arab Emirates"~ "ARE",
+  Country == "India"~ "IND",
+  Country == "United States"~ "USA",
+  Country == "Sweden"~ "SWE",
+  Country == "China"~ "CHN",
+  Country == "Singapore"~ "SGP",
+  Country == "New Zealand"~ "NZL",
+  Country == "SPain"~ "ESP",
+  Country == "Russia"~ "RUS",
+  Country == "Ireland"~ "IRL",
+  Country == "Japan"~ "JPN",
+  Country == "Greece"~ "GRC",
+  Country == "Belgium"~ "BEL",
+  Country == "Ecuador"~ "ECU",
+  Country == "Mexico"~ "MEX",
+  Country == "Turkey"~ "TUR",
+  Country == "Switzerland"~ "CHE",
+  Country == "Quatar"~ "QAT",
+  Country == "Belgium"~ "BEL",
+  Country == "Denmark"~ "DNK",
+  Country == "Germany"~ "DEU",
+  Country == "Poland"~ "POL",
+  Country == "Netherland"~ "NLD",
+  Country == "Italy"~ "ITA",
+  Country == "Portugal"~ "PRT",
+  Country == "Hungary"~ "HUN",
+  Country == "Lithuania"~ "LTU",
+  Country == "Malaysia"~ "MYS",
+  Country == "Thailand"~ "THA",
+  Country == "Finland"~ "FIN",
+  Country == "Virginia"~ "VIR",
+  Country == "Bulgaria"~ "BGR",
+  Country == "Ireland"~ "IRL",
+  Country == "Tunisia"~ "TUN",
+  Country == "Romania" ~ "ROU",
+  Country == "Czech Replublic"~ "CZE",
+  Country == "Malta"~ "MLT"
+))
+
+# Education qualification
+
+data1 <- data1 %>% unite("Educational_qualifications",c("BSc_needed","MSc_needed", "PhD_needed"))
+data1
+data1$Educational_qualifications <- gsub(".*0_0_0.*",NA,data1$Educational_qualifications)
+
+# Knowledge In 
+
+data1 <- data1 %>% unite("Knowledge_in")
 
 # Export dataset
 
